73b6dbb8574bd8b528d74c994867e43a81e3481b
Kumaraditya Gupta <kumaraditya511@gmail.com>
2023-09-08 22:34:24 +0530

    Script added to create pose folder

---
+++ create_poses.py
@@ -0,0 +1,19 @@
+import os
+from tqdm import tqdm
+
+poses_file_path = "/Users/kumaraditya/Desktop/run_kinect_wheel_1/poses.txt"
+output_path = "/Users/kumaraditya/Desktop/run_kinect_wheel_1/pose/"
+
+poses_file = open(poses_file_path, "r")
+poses_file.readline()
+
+for line in tqdm(poses_file):
+    data = line.split(" ")
+    id = data[8]
+    # id has a \n at the end
+    id = id[:-1]
+    output_file = open(output_path + id + ".txt", "a")
+    output_file.write(data[1] + " " + data[2] + " " + data[3] + " " + data[4] + " " + data[5] + " " + data[6] + " " + data[7])
+    output_file.close()
+
+poses_file.close()
