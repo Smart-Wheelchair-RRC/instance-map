4dfaf98050dd207f63cbbdf2f11e06e51fbcdd58
Kumaraditya Gupta <kumaraditya511@gmail.com>
2023-10-07 17:07:24 +0530

    Run on iphone and ipad data also

--- create_poses.py
+++ create_poses.py
@@ -1,8 +1,8 @@
 import os
 from tqdm import tqdm
 
-poses_file_path = "/scratch/kumaradi.gupta/run_ipad/poses.txt"
-output_path = "/scratch/kumaradi.gupta/run_ipad/pose/"
+poses_file_path = "/scratch/kumaradi.gupta/run_iphone/poses.txt"
+output_path = "/scratch/kumaradi.gupta/run_iphone/pose/"
 
 poses_file = open(poses_file_path, "r")
 poses_file.readline()
